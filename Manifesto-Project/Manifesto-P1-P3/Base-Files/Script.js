874ca98777053d90d8d3f4447eeda75b13730c5d
Daniel Reagan <64659829+mrstarwars18@users.noreply.github.com>
2021-12-04 13:28:09 -0600

    Add files via upload

---
+++ Manifesto-Project/Manifesto-P1-P3/Base-Files/Script.js
@@ -0,0 +1,9 @@
+function push1(){
+window.location.assign("ManifestoV2.html")
+}
+function push2(){
+window.location.assign("ManifestoV3.html")
+}
+function push3(){
+window.location.assign("ManifestoV4.html")
+}
